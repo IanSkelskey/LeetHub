1af37a8eeaf58ab27704c35a07631d010027b273
Ian Skelskey <46094112+IanSkelskey@users.noreply.github.com>
2022-07-26 01:33:16 -0400

    Time: 558 ms (38.17%), Space: 0B (100.00%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product
+        ORDER BY product
+        SEPARATOR ',') AS 'products'
+FROM
+    Activities
+GROUP BY sell_date
